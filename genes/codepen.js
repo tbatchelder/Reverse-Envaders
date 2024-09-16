8478f1a7ed7bc9cdc41b14ea61ce6ee30b0517c5
tbatchelder <pegacorn@optonline.net>
2024-09-15 21:18:55 -0400

    Removed all BrickBreaker code

--- genes/codepen.js
+++ genes/codepen.js
@@ -213,6 +213,7 @@ function buildPShieldList() {
 //------------------------------
 function drawBG() {
   CTX.fillStyle = BGCOLOR;
+  CTX.fillRect(0, 0, 1000, 800);
 }
 function drawGround() {
   CTX.fillStyle = "brown";
@@ -334,36 +335,36 @@ function drawPlayerShield(x, y) {
 
 
 // used to draw the ball
-function circle(x, y, r) {
-  CTX.beginPath();
-  CTX.arc(x, y, r, 0, Math.PI * 2, true);
-  CTX.closePath();
-  CTX.fill();
-}
+//function circle(x, y, r) {
+//  CTX.beginPath();
+//  CTX.arc(x, y, r, 0, Math.PI * 2, true);
+//  CTX.closePath();
+//  CTX.fill();
+//}
 
 // used to draw each brick & the paddle
-function rect(x, y, w, h) {
-  CTX.beginPath();
-  CTX.rect(x, y, w, h);
-  CTX.closePath();
-  CTX.fill();
-}
+//function rect(x, y, w, h) {
+//  CTX.beginPath();
+//  CTX.rect(x, y, w, h);
+//  CTX.closePath();
+//  CTX.fill();
+//}
 
 // clear the screen in between drawing each animation
 function clear() {
   CTX.clearRect(0, 0, width, height);
-  rect(0, 0, width, height);
+  //rect(0, 0, width, height);
 }
 
 // What do to when the mouse moves within the canvas
-function onMouseMove(evt) {
-  // set the paddle position if the mouse position 
-  // is within the borders of the canvas
-  if (evt.pageX > canvasMinX && evt.pageX < canvasMaxX) {
-    paddlex = Math.max(evt.pageX - canvasMinX - (paddlew / 2), 0);
-    paddlex = Math.min(width - paddlew, paddlex);
-  }
-}
+//function onMouseMove(evt) {
+// set the paddle position if the mouse position 
+// is within the borders of the canvas
+//if (evt.pageX > canvasMinX && evt.pageX < canvasMaxX) {
+//paddlex = Math.max(evt.pageX - canvasMinX - (paddlew / 2), 0);
+//paddlex = Math.min(width - paddlew, paddlex);
+//}
+//}
 
 function onKeyDown(evt) {
   evt.preventDefault();
@@ -449,79 +450,79 @@ function pause() {
 }
 
 // initialize array of bricks to be visible (true)
-function init_bricks() {
-  bricks = new Array(nrows);
-  brickColor = new Array(nrows);
-  for (i = 0; i < nrows; i++) { // for each row of bricks
-    bricks[i] = new Array(ncols);
-    brickColor[i] = new Array(ncols);
-    for (j = 0; j < ncols; j++) { // for each column of bricks
-      bricks[i][j] = true;
-      brickColor[i][j] = "";
-    }
-  }
-}
+//function init_bricks() {
+//bricks = new Array(nrows);
+//brickColor = new Array(nrows);
+//for (i = 0; i < nrows; i++) { // for each row of bricks
+//bricks[i] = new Array(ncols);
+//brickColor[i] = new Array(ncols);
+//for (j = 0; j < ncols; j++) { // for each column of bricks
+//bricks[i][j] = true;
+//brickColor[i][j] = "";
+//}
+//}
+//}
 
 // render the bricks
-function draw_bricks() {
-  for (i = 0; i < nrows; i++) { // for each row of bricks
-    for (j = 0; j < ncols; j++) { // for each column of bricks
-      // set the colors to alternate through
-      // all colors in brick_colors array
-      // modulus (%, aka remainder) ensures the colors
-      // rotate through the whole range of brick_colors
-      CTX.fillStyle = brick_colors[(i + j) % brick_colors.length];
-      brickColor[i][j] = brick_colors[(i + j) % brick_colors.length];
-      if (bricks[i][j]) {
-        rect((j * (brickWidth + padding)) + padding,
-          (i * (brickHeight + padding)) + padding,
-          brickWidth, brickHeight);
-      }
-    }
-  }
-}
+//function draw_bricks() {
+//for (i = 0; i < nrows; i++) { // for each row of bricks
+//for (j = 0; j < ncols; j++) { // for each column of bricks
+// set the colors to alternate through
+// all colors in brick_colors array
+// modulus (%, aka remainder) ensures the colors
+// rotate through the whole range of brick_colors
+//CTX.fillStyle = brick_colors[(i + j) % brick_colors.length];
+//brickColor[i][j] = brick_colors[(i + j) % brick_colors.length];
+//if (bricks[i][j]) {
+//rect((j * (brickWidth + padding)) + padding,
+//(i * (brickHeight + padding)) + padding,
+//brickWidth, brickHeight);
+//}
+//}
+//}
+//}
 
 
 
 
 
-var x = 200;      // starting horizontal position of ball
-var y = 150;      // starting vertical position of ball
-var dx = 1;       // amount ball should move horizontally
-var dy = -3;      // amount ball should move vertically
+//var x = 200;      // starting horizontal position of ball
+//var y = 150;      // starting vertical position of ball
+//var dx = 1;       // amount ball should move horizontally
+//var dy = -3;      // amount ball should move vertically
 // variables set in init()
-var width, height, paddlex, bricks, brickWidth;
-var paddleh = 10; // paddle height (pixels)
-var paddlew = 75; // paddle width (pixels)
+var width, height;//, paddlex, bricks, brickWidth;
+//var paddleh = 10; // paddle height (pixels)
+//var paddlew = 75; // paddle width (pixels)
 var canvasMinX = 0; // minimum canvas x bounds
 var canvasMaxX = 0; // maximum canvas x bounds
 var intervalId = 0; // track refresh rate for calling draw()
-var nrows = 6; // number of rows of bricks
-var ncols = 8; // number of columns of bricks
-var brickHeight = 15; // height of each brick
-var padding = 1;  // how far apart bricks are spaced
+//var nrows = 6; // number of rows of bricks
+//var ncols = 8; // number of columns of bricks
+//var brickHeight = 15; // height of each brick
+//var padding = 1;  // how far apart bricks are spaced
 var paused = false; // keeps track of whether the game is paused (true) or not (false)
-var ballRadius = 10; // size of ball (pixels)
+//var ballRadius = 10; // size of ball (pixels)
 // change colors of bricks -- add as many colors as you like
-var brick_colors = ["burlywood", "chocolate", "firebrick", "midnightblue"];
-var paddlecolor = "black";
-var ballcolor = "black";
+//var brick_colors = ["burlywood", "chocolate", "firebrick", "midnightblue"];
+//var paddlecolor = "black";
+//var ballcolor = "black";
 
 
-var next = 20;
-var direction = "right";
+//var next = 20;
+//var direction = "right";
 
 // initialize game
 function init() {
   width = 1000;
   height = 800;
-  paddlex = width / 2;
-  brickWidth = (width / ncols) - 1;
-  canvasMinX = 0;
-  canvasMaxX = canvasMinX + width;
+  //paddlex = width / 2;
+  //brickWidth = (width / ncols) - 1;
+  //canvasMinX = 0;
+  //canvasMaxX = canvasMinX + width;
   // run draw function every 10 milliseconds to give 
   // the illusion of movement
-  init_bricks();
+  //init_bricks();
   buildECityList();
   buildEGearList();
   buildEShieldList(4, 0);
@@ -535,19 +536,20 @@ function init() {
 
 function reload() {
   stop_animation(); // clear out the animation - it's cause the ball to speed up
-  x = 200;      // starting horizontal position of ball
-  y = 150;      // starting vertical position of ball
-  dx = 1;       // amount ball should move horizontally
-  dy = -3;      // amount ball should move vertically
-  score = 0;
-  next = 0;
+  //x = 200;      // starting horizontal position of ball
+  //y = 150;      // starting vertical position of ball
+  //dx = 1;       // amount ball should move horizontally
+  //dy = -3;      // amount ball should move vertically
+  //score = 0;
+  //next = 0;
   init();
 }
 
 function draw() {
   // before drawing, change the fill color
-  drawBG();
   clear();
+  drawBG();
+
   drawGround();
   drawSky();
   drawCity();
@@ -581,7 +583,7 @@ function draw() {
 
   // Check enemy energy level
   for (let i = 0; i < 10; i++) {
-    if ((865 - eGearEnergy) > GEARLEVELLIST[i]) {
+    if ((855 - eGearEnergy) >= GEARLEVELLIST[i]) {
       break;
     } else {
       eGearList[i][4] = "gold";
@@ -590,7 +592,7 @@ function draw() {
 
   // Check player energy level
   for (let i = 0; i < 10; i++) {
-    if ((865 - pEnergy) > GEARLEVELLIST[i]) {
+    if ((855 - pEnergy) > GEARLEVELLIST[i]) {
       break;
     } else {
       pGearList[i][4] = "gold";
@@ -629,89 +631,89 @@ function draw() {
   }
 
 
-  CTX.fillStyle = ballcolor;
+  //CTX.fillStyle = ballcolor;
   //draw the ball
-  circle(x, y, ballRadius);
-  CTX.fillStyle = paddlecolor;
+  //circle(x, y, ballRadius);
+  //CTX.fillStyle = paddlecolor;
   //draw the paddle
-  rect(paddlex, height - paddleh, paddlew, paddleh);
-  draw_bricks();
+  //rect(paddlex, height - paddleh, paddlew, paddleh);
+  //draw_bricks();
   drawWall();
 
 
   //check if we have hit a brick
-  rowheight = brickHeight + padding;
-  colwidth = brickWidth + padding;
-  row = Math.floor(y / rowheight);
-  col = Math.floor(x / colwidth);
+  //rowheight = brickHeight + padding;
+  //colwidth = brickWidth + padding;
+  //row = Math.floor(y / rowheight);
+  //col = Math.floor(x / colwidth);
   //if so reverse the ball and mark the brick as broken
-  if (y < nrows * rowheight && row >= 0 && col >= 0 && bricks[row][col]) {
-    dy = -dy;
-    bricks[row][col] = false;
-    //"burlywood", "chocolate", "firebrick", "midnightblue"
-    switch (brickColor[row][col]) {
-      case "burlywood":
-        score = score + 10;
-        break;
-      case "chocolate":
-        score = score + 20;
-        break;
-      case "firebrick":
-        score = score + 5;
-        break;
-      case "midnightblue":
-        score = score + 1;
-        break;
-      default:
-      // code block
-    }
-    //score = score + 1;
-    update_score_text();
-  }
+  //if (y < nrows * rowheight && row >= 0 && col >= 0 && bricks[row][col]) {
+  //dy = -dy;
+  //bricks[row][col] = false;
+  //"burlywood", "chocolate", "firebrick", "midnightblue"
+  //switch (brickColor[row][col]) {
+  //case "burlywood":
+  //score = score + 10;
+  //break;
+  //case "chocolate":
+  //score = score + 20;
+  //break;
+  //case "firebrick":
+  //score = score + 5;
+  //break;
+  //case "midnightblue":
+  //score = score + 1;
+  //break;
+  //default:
+  // code block
+  //}
+  //score = score + 1;
+  //update_score_text();
+  //}
 
   //contain the ball by rebouding it off the walls of the canvas
-  if (x + dx > width || x + dx < 0)
-    dx = -dx;
+  //if (x + dx > width || x + dx < 0)
+  //dx = -dx;
 
-  if (y + dy < 0) {
-    dy = -dy;
-  } else if (y + dy > height - paddleh) {
-    // check if the ball is hitting the 
-    // paddle and if it is rebound it
-    if (x > paddlex && x < paddlex + paddlew) {
-      dy = -dy;
-    }
-  }
-  if (y + dy > height) {
-    //game over, so stop the animation
-    stop_animation();
-  }
-  x += dx;
-  y += dy;
-  if (next > 20) {
-    direction = "left";
-  }
-  if (next < 0) {
-    direction = "right";
-  }
-  if (direction = "right") {
-    next += 2;
-  } else {
-    next = next - 2;
-  }
+  //if (y + dy < 0) {
+  //dy = -dy;
+  //} else if (y + dy > height - paddleh) {
+  // check if the ball is hitting the 
+  // paddle and if it is rebound it
+  //if (x > paddlex && x < paddlex + paddlew) {
+  //dy = -dy;
+  //}
+  //}
+  //if (y + dy > height) {
+  //game over, so stop the animation
+  //stop_animation();
+  //}
+  //x += dx;
+  //y += dy;
+  //if (next > 20) {
+  //direction = "left";
+  //}
+  //if (next < 0) {
+  //direction = "right";
+  //}
+  //if (direction = "right") {
+  //next += 2;
+  //} else {
+  //next = next - 2;
+  //}
 }
 
-function update_score_text() {
-  // You can send data to your HTML
-  // just like setting styles in CSS
-  // Put <div id="score"></div> in
-  // your HTML for thjis text to display
-  //$('#score').text("Score: " + score);
-}
+//function update_score_text() {
+// You can send data to your HTML
+// just like setting styles in CSS
+// Put <div id="score"></div> in
+// your HTML for thjis text to display
+//$('#score').text("Score: " + score);
+//}
 
 function start_animation() {
   intervalId = setInterval(draw, 10);
-  update_score_text();
+  //update_score_text();
 }
 
 function stop_animation() {
@@ -730,7 +732,7 @@ theGame = document.getElementById("myCanvas");
 theGame.addEventListener('keydown', function (event) {
   onKeyDown(event);
 }, false);
-theGame.addEventListener('mousemove', function (event) {
-  onMouseMove(event);
-}, false);
+//theGame.addEventListener('mousemove', function(event) {
+//onMouseMove(event);
+//}, false);
 init();                             // initialize & begin game
